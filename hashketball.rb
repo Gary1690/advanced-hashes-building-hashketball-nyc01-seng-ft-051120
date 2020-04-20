defb16e1715fbd0f36e83b82dcbbfcaf5caa9b59
Gary Cordero Rosa <garycordero1690@gmail.com>
2020-04-20 16:59:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,20 @@
 def game_hash 
  game = {
    :home =>{
-     :team_name =>"",
-     :colors =>[],
+     :team_name =>"Brooklyn Nets",
+     :colors =>["Black","White"],
      :players => [
+       {
+         :player_name =>"Alan Anderson",
+         :number =>0,
+         :shoe =>16,
+         :points =>22,
+         :rebounds =>12,
+         :assists =>12,
+         :steals =>3,
+         :blocks =>1,
+         :slam_dunks =>1
+       },
        {
          :player_name =>,
          :number =>,
@@ -14,7 +25,7 @@ def game_hash
          :assists =>,
          :steals =>,
          :blocks =>,
-         :slam_dunks =>,
+         :slam_dunks =>
        },
        ]
    },
