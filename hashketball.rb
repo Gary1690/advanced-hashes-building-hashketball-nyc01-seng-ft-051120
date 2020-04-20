b78048feadcffa83c79360303e46df947c2ba05a
Gary Cordero Rosa <garycordero1690@gmail.com>
2020-04-20 16:57:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,26 @@
 # Write your code here!
 def game_hash 
+ game = {
+   :home =>{
+     :team_name =>"",
+     :colors =>[],
+     :players => [
+       {
+         :player_name =>,
+         :number =>,
+         :shoe =>,
+         :points =>,
+         :rebounds =>,
+         :assists =>,
+         :steals =>,
+         :blocks =>,
+         :slam_dunks =>,
+       },
+       ]
+   },
+   :away => {
+     
+   }
+ }
+end
   
